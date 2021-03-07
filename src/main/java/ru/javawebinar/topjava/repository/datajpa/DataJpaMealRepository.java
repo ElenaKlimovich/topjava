dcac84a67b0bb94ab1e2b3d889a3df5c882de6e8
ElenaKlimovich <elenasunandsea@gmail.com>
2021-03-07 23:04:17 +0300

    HW05 - implementing DataJpaMealRepository, MealServiceTest - OK!

--- src/main/java/ru/javawebinar/topjava/repository/datajpa/DataJpaMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/datajpa/DataJpaMealRepository.java
@@ -2,6 +2,7 @@ package ru.javawebinar.topjava.repository.datajpa;
 
 import org.springframework.stereotype.Repository;
 import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.MealRepository;
 
 import java.time.LocalDateTime;
@@ -11,33 +12,40 @@ import java.util.List;
 public class DataJpaMealRepository implements MealRepository {
 
     private final CrudMealRepository crudRepository;
+    private final DataJpaUserRepository dataJpaUserRepository;
 
-    public DataJpaMealRepository(CrudMealRepository crudRepository) {
+    public DataJpaMealRepository(CrudMealRepository crudRepository, DataJpaUserRepository dataJpaUserRepository) {
         this.crudRepository = crudRepository;
+        this.dataJpaUserRepository = dataJpaUserRepository;
     }
 
     @Override
     public Meal save(Meal meal, int userId) {
-        return null;
+        if(!meal.isNew() && get(meal.id(), userId) == null) {
+            return null;
+        }
+        User user = dataJpaUserRepository.get(userId);
+        meal.setUser(user);
+        return crudRepository.save(meal);
     }
 
     @Override
     public boolean delete(int id, int userId) {
-        return false;
+        return crudRepository.delete(id, userId) != 0;
     }
 
     @Override
     public Meal get(int id, int userId) {
-        return null;
+        return crudRepository.get(id, userId);
     }
 
     @Override
     public List<Meal> getAll(int userId) {
-        return null;
+        return crudRepository.getAll(userId);
     }
 
     @Override
     public List<Meal> getBetweenHalfOpen(LocalDateTime startDateTime, LocalDateTime endDateTime, int userId) {
-        return null;
+        return crudRepository.getBetweenHalfOpen(startDateTime, endDateTime, userId);
     }
 }
